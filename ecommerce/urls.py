0b97784071f7a27836554c0545efd6833b7f8c22
marcelokfws <marcelokfws@gmail.com>
2023-10-30 23:46:33 -0300

    login configuration

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
     path('cart/', include('cart.urls')),
+    path('account/', include('account.urls')),
 
 ]
 
