5b2a23cc6f0a1e036950bea7bca1d1a0dbb1c730
marcelokfws <marcelokfws@gmail.com>
2023-11-04 01:45:54 -0300

    send_email

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.core.mail import send_mail
 from django.http import JsonResponse
 from django.shortcuts import render
 
@@ -89,12 +91,26 @@ def complete_order(request):
                                          amount_paid=total_cost, user=request.user)
 
             order_id = order.pk
+            product_list = [' Produto x Quantidade: ']
+
+            full_name = name
 
             for item in cart:
 
                 OrderItem.objects.create(order_id=order_id, product=item['product'], quantity=item['qty'],
 
                                          price=item['price'], user=request.user)
+
+                product_list.append(item['product'])
+                product_list.append(item['qty'])
+                all_products = product_list
+
+            send_mail('Pedido recebido', 'Olá, ' + str(full_name) + '\n\n' + 'Obrigado por fazer seu pedido' + '\n\n' +
+                      'Por favor, veja seu pedido abaixo:' +
+                      '\n\n' + str(all_products) + '\n\n' +
+                      'Total pago R$' +
+                      str(cart.get_total()), settings.EMAIL_HOST_USER, [email], fail_silently=False,
+                      )
 
         #  2) Create order -> Guest users without an account
 
